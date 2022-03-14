6de85c13ef8153eb627e0255dc73b2b89d9306df
Lenny B_home <kozmonodt@gmail.com>
2022-03-14 19:49:03 +0300

    14 lesson done

---
+++ app/Http/Controllers/MyTestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyTestController extends Controller
+{
+    public function index()
+    {
+        return 'satanic test shit';
+    }
+}
