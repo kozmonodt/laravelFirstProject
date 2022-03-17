2b5ceddbb0dd4e79332e1c50bc982d4e72db3c80
ABulah <kozmonodt@gmail.com>
2022-03-17 16:54:10 +0300

    18 lesson done

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,19 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="{{route('main.index')}}">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="{{route('posts.index')}}">Posts</a>
+                    <a class="nav-item nav-link" href="{{route('about.index')}}">About</a>
+                    <a class="nav-item nav-link" href="{{route('contact.index')}}">Contacts</a>
+                </div>
+            </div>
+        </nav>
+{{--        <nav>
             <ul>
                 <li><a href="{{route('main.index')}}">Main</a></li>
                 <li><a href="{{route('posts.index')}}">Posts</a></li>
                 <li><a href="{{route('about.index')}}">About</a></li>
                 <li><a href="{{route('contact.index')}}">Contacts</a></li>
             </ul>
-        </nav>
+        </nav>--}}
     </div>
     @yield('content')
 </div>
